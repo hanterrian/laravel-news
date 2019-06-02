4de84cc9dd485360a4b575df21ac819bcdbd71ce
hanterrian <hanterrian@ukr.net>
2019-06-02 12:53:52 +0300

    Add store model

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 
-@section('title', 'Page Title')
+@section('title', setting('site.title'))
 
 @section('header')
     <header class="header">
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-md-3">
                         <a href="#">
-                            <img src="/templates/zaim24/img/logotype.png" alt="" class="img-responsive header__logotype">
+                            <img src="{{ asset('img/logotype.png') }}" alt="" class="img-responsive header__logotype">
                         </a>
                     </div>
                     <div class="col-md-9">
@@ -27,46 +27,45 @@
         <h2 class="offers__title">Предложения Украины</h2>
         <div class="row">
             @foreach($models as $model)
-            @endforeach
-            @for ($i = 0; $i < 10; $i++)
                 <div class="col col-lg-3 col-md-4 col-sm-6">
                     <div class="offer">
-                        <a href="http://pix15.link/scripts/click.php?ban_id=h12dq8jz&amp;aff_id=88888888" target="_blank">
-                            <div style="background-image:url(//arbitraff.ru/img/offers_logos/366.png)" class="offer__logo"></div>
+                        <a href="{{ $model->link }}" target="_blank">
+                            <div style="background-image:url({{ Voyager::image($model->logo) }})" class="offer__logo"></div>
                         </a>
                         <table class="offer__table">
                             <tbody>
                             <tr>
                                 <td class="offer__table-td--left">Сумма</td>
-                                <td class="offer__table-td--right">to 15000 грн</td>
+                                <td class="offer__table-td--right"> {{ $model->sum }} грн</td>
                             </tr>
                             <tr>
                                 <td class="offer__table-td--left">Ставка</td>
-                                <td class="offer__table-td--right">от 0% в день</td>
+                                <td class="offer__table-td--right">от {{ $model->rate }}% в день</td>
                             </tr>
                             <tr>
                                 <td class="offer__table-td--left">Срок</td>
                                 <td class="offer__table-td--right"> от
-                                    <span> 62</span>
+                                    <span> {{ $model->term_from }}</span>
                                     до
-                                    <span> 365</span>
+                                    <span> {{ $model->term_to }}</span>
                                     дней
                                 </td>
                             </tr>
                             </tbody>
                         </table>
                         <div class="offer__hit-wrap">
-                            <p class="offer__hit">Только до 31 мая!</p>
-                            <p class="offer__hit">Автовыдача 24/7!</p>
+                            @foreach(explode('|', $model->description) as $description)
+                                <p class="offer__hit">{{ $description }}</p>
+                            @endforeach
                         </div>
                         <div class="offer__button-wrap">
-                            <a href="http://pix15.link/scripts/click.php?ban_id=h12dq8jz&amp;aff_id=88888888" target="_blank" class="offer__button">
+                            <a href="{{ $model->link }}" target="_blank" class="offer__button">
                                 ПОЛУЧИТЬ ДЕНЬГИ
                             </a>
                         </div>
                     </div>
                 </div>
-            @endfor
+            @endforeach
         </div>
     </section>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <meta name="description" content="{{ setting('site.description') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ setting('site.logo') }}"/>
 </head>
 <body>
 @yield('header')
