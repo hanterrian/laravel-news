bae37effc9473ccc3844df4118ae7b6ff3cdc832
hanterrian <hanterrian@ukr.net>
2019-06-15 09:43:00 +0300

    Fix link target

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,52 +11,52 @@ ScrollReveal().reveal('[data-scrollreveal]');
 
 // Toggle the header search bar.
 $(document).on('click', '[data-toggle-search-trigger]', (e) => {
-  e.preventDefault();
-  $('[data-toggle-search]').slideToggle();
+    e.preventDefault();
+    $('[data-toggle-search]').slideToggle();
 });
 
 // Prefetch and transition to the next page.
 (() => {
-  'use strict';
+    'use strict';
 
-  // Prefetch a url on hover.
-  $(document).on('mouseover', 'a', (e) => {
-    // New prerender url.
-    const url = $(e.currentTarget).attr('href');
+    // Prefetch a url on hover.
+    // $(document).on('mouseover', 'a', (e) => {
+    //   // New prerender url.
+    //   const url = $(e.currentTarget).attr('href');
+    //
+    //   if (url && url.length > 1) {
+    //     // Prerender <Link> element.
+    //     const prerender = $('#prerender');
+    //
+    //     // Create a new <link> element and apply the prerender url.
+    //     if (prerender.length === 0) {
+    //       $('<link id="prerender" rel="prefetch prerender" href="' + url + '">').appendTo('body');
+    //
+    //       return;
+    //     }
+    //
+    //     // Prerender already contains the hovered link url.
+    //     if (prerender.attr('href') === url) {
+    //       return;
+    //     }
+    //
+    //     prerender.attr('href', url);
+    //   }
+    // });
 
-    if (url && url.length > 1) {
-      // Prerender <Link> element.
-      const prerender = $('#prerender');
-
-      // Create a new <link> element and apply the prerender url.
-      if (prerender.length === 0) {
-        $('<link id="prerender" rel="prefetch prerender" href="' + url + '">').appendTo('body');
-
-        return;
-      }
-
-      // Prerender already contains the hovered link url.
-      if (prerender.attr('href') === url) {
-        return;
-      }
-
-      prerender.attr('href', url);
-    }
-  });
-
-  // Transition out the current page.
-  $(document).on('click', 'a', (e) => {
-    // Redirect url.
-    const url = $(e.currentTarget).attr('href');
-
-    if (url && url.length > 1) {
-      e.preventDefault();
-
-      $('body').addClass('fadeOut');
-
-      setTimeout(() => {
-        window.location.href = url;
-      }, 150);
-    }
-  });
+    // Transition out the current page.
+    // $(document).on('click', 'a', (e) => {
+    //   // Redirect url.
+    //   const url = $(e.currentTarget).attr('href');
+    //
+    //   if (url && url.length > 1) {
+    //     e.preventDefault();
+    //
+    //     $('body').addClass('fadeOut');
+    //
+    //     setTimeout(() => {
+    //       window.location.href = url;
+    //     }, 150);
+    //   }
+    // });
 })();
