f778e733dca297a8257fc4be657f1d860c1c26e8
hanterrian <hanterrian@ukr.net>
2019-07-14 21:43:47 +0300

    Add cors

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -24,6 +24,22 @@ class ScriptController extends Controller
 
         $js = <<<JS
 console.log('Init scripts');
+
+if(!window.jQuery)
+{
+    console.log('jQuery not found? init it');
+    
+    var script = document.createElement('script');
+    
+    script.type = "text/javascript";
+    script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+    script.integrity = "sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=";
+    script.crossorigin = "anonymous";
+    
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+console.log('Init scripts complete');
 JS;
 
         return (new Response($js, 200))->header('Content-type', 'application/javascript');
