68bef48a636324323a067076152cde4cbfd16b1d
hanterrian <hanterrian@ukr.net>
2019-08-11 22:17:28 +0300

    Add popup backend

--- database/migrations/2019_08_03_074611_mod_table_popup.php
+++ database/migrations/2019_08_03_074611_mod_table_popup.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModTablePopup extends Migration
+class ModTablePopup2 extends Migration
 {
     /**
      * Run the migrations.
