9c8ad673c7d48d4a4acc86e15563220706d81638
hanterrian <hanterrian@ukr.net>
2019-08-26 15:35:48 +0300

    Add email sender

--- resources/views/script/banner-one.blade.php
+++ resources/views/script/banner-one.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="b-frames">
-        <a class="b-frame" href="{{ $popup->url_1 }}" target="_blank">
+        <a class="b-frame b-frame-width" href="{{ $popup->url_1 }}" target="_blank">
             <img class="b-img" src="{{ Voyager::image($popup->image_1) }}" alt="{{ $popup->text_1 }}"/>
             <div class="b-frame-text">
                 {{ $popup->text_1 }}
