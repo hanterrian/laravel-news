9f94540557706e16ae3c873e499dac6d79fecef1
hanterrian <hanterrian@ukr.net>
2019-08-26 15:50:49 +0300

    Add email sender

--- resources/views/script/banner-layout.blade.php
+++ resources/views/script/banner-layout.blade.php
@@ -13,11 +13,15 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
+        var bodyHeight = parent.document.body.clientHeight;
+
+        $('.b-frames').height(bodyHeight);
+
         $.each($('.b-frame'), function (k, item) {
             var width = $(item).width();
             var height = $(item).height();
             var img = $(item).find('img');
-            
+
             img.width(width).height(height);
         })
     });
