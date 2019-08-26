e3b95ac2e34e1ae32520bba6efcbd34368a31127
hanterrian <hanterrian@ukr.net>
2019-08-26 15:32:19 +0300

    Add email sender

--- resources/views/script/banner-three.blade.php
+++ resources/views/script/banner-three.blade.php
@@ -6,8 +6,7 @@
 
     <div class="b-frames">
         @if($popup->url_1)
-            <a class="b-frame b-frame-1" href="{{ $popup->url_1 }}" target="_blank">
-                <img class="b-img b-img-1" src="{{ Voyager::image($popup->image_1) }}" alt="{{ $popup->text_1 }}"/>
+            <a class="b-frame b-frame-1" href="{{ $popup->url_1 }}" target="_blank" style="background: url('{{ Voyager::image($popup->image_1) }}') no-repeat; background-size: contain">
                 <div class="b-frame-text">
                     {{ $popup->text_1 }}
                 </div>
@@ -15,8 +14,7 @@
         @endif
 
         @if($popup->url_2)
-            <a class="b-frame b-frame-2" href="{{ $popup->url_2 }}" target="_blank">
-                <img class="b-img b-img-2" src="{{ Voyager::image($popup->image_2) }}" alt="{{ $popup->text_2 }}"/>
+            <a class="b-frame b-frame-2" href="{{ $popup->url_2 }}" target="_blank" style="background: url('{{ Voyager::image($popup->image_2) }}') no-repeat; background-size: contain">
                 <div class="b-frame-text">
                     {{ $popup->text_2 }}
                 </div>
@@ -24,8 +22,7 @@
         @endif
 
         @if($popup->url_3)
-            <a class="b-frame b-frame-3" href="{{ $popup->url_3 }}" target="_blank">
-                <img class="b-img b-img-3" src="{{ Voyager::image($popup->image_3) }}" alt="{{ $popup->text_3 }}"/>
+            <a class="b-frame b-frame-3" href="{{ $popup->url_3 }}" target="_blank" style="background: url('{{ Voyager::image($popup->image_3) }}') no-repeat; background-size: contain">
                 <div class="b-frame-text">
                     {{ $popup->text_3 }}
                 </div>
