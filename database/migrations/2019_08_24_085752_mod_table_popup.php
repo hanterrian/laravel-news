e8bcc4e128dc3f321927297b0ad7a8cb6338757b
hanterrian <hanterrian@ukr.net>
2020-05-09 11:40:12 +0300

    Update project

--- database/migrations/2019_08_24_085752_mod_table_popup.php
+++ database/migrations/2019_08_24_085752_mod_table_popup.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModTablePopup extends Migration
+class ModTablePopup3 extends Migration
 {
     /**
      * Run the migrations.
