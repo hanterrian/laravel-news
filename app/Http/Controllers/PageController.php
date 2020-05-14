3d3b07330952da626f790f8af2ddf2db5854712a
hanterrian <hanterrian@ukr.net>
2020-05-14 22:46:21 +0300

    Update project

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Page;
 use App\PageBlock;
 use Illuminate\Http\Request;
-use Pvtl\VoyagerPageBlocks\Traits\Blocks;
 
 /**
  * Class PageController
@@ -14,8 +13,6 @@ use Pvtl\VoyagerPageBlocks\Traits\Blocks;
  */
 class PageController extends Controller
 {
-    use Blocks;
-
     /**
      * @param $slug
      *
@@ -24,28 +21,6 @@ class PageController extends Controller
     public function view($slug)
     {
         $page = Page::whereSlug($slug)->firstOrFail();
-
-        $blocks = $page->blocks()
-            ->where('is_hidden', '=', '0')
-            ->orderBy('order', 'asc')
-            ->get()
-            ->map(function (PageBlock $block) {
-                return (object)[
-                    'id' => $block->id,
-                    'page_id' => $block->page_id,
-                    'updated_at' => $block->updated_at,
-                    'cache_ttl' => $block->cache_ttl,
-                    'template' => $block->template()->template,
-                    'data' => $block->cachedData,
-                    'path' => $block->path,
-                    'type' => $block->type,
-                ];
-            });
-
-        $page['body'] = view('voyager-page-blocks::default', [
-            'page' => $page,
-            'blocks' => $this->prepareEachBlock($blocks),
-        ]);
 
         return view('page.view', ['page' => $page]);
     }
